fixing wound mask scaling problem

main/views.py
@@ -63,26 +63,28 @@ def index(request):
         if 'myfile' in request.FILES:
             files = request.FILES.get('myfile').read()
             image = image_save(files,str_time)
-            opencv_image = np.array(image).copy()[:,:,::-1]
-            print("opencv_image.shape : ",opencv_image.shape)
+            opencv_image = np.array(image).copy()
             result = detect_mask_single_image_using_grpc(opencv_image,str_time)
 
             ori_file_folder = os.path.join(MEDIA_DIR,str_time+'.jpg')
             cropped_file_folder = None
             file_name= str_time+".jpg"
             if result:
-                predict_result = visualize.save_image(image, "test", result['rois'], result['mask'],
+                predict_result = visualize.save_image(opencv_image, None, result['rois'], result['mask'],
                     result['class'], result['scores'], class_names,scores_thresh=0.85)
                 if predict_result:
-                    print("predict_result : ",predict_result.shape)
                     cropped_file_folder =  os.path.join(MEDIA_DIR,str_time+'-cropped.jpg')
-                    bounding_box = result['rois']
-                    mask = result['mask']
-                    bounding_box,mask = process_bounding_mask(bounding_box,mask)
-                    x1,y1,x2,y2 = bounding_box
-                    opencv_image = opencv_image[y1:y2,x1:x2]
-                    cv2.imwrite(cropped_file_folder,opencv_image)
-                    cv2.imwrite(ori_file_folder,predict_result)
+                    print("rois : ",result['rois'].shape)
+                    bounding_box = result['rois'][0].copy()
+                    mask = result['mask'][...,0].copy()
+                    mask = 255.0*mask
+                    result['rois'][0],mask = process_bounding_mask(bounding_box,mask)
+                    x1,y1,x2,y2 = result['rois'][0]
+                    cropped_image = opencv_image[y1:y2,x1:x2].copy()[:,:,::-1]
+                    cv2.imwrite(cropped_file_folder,cropped_image)
+
+                    visualize.save_image(opencv_image, ori_file_folder, result['rois'], mask,
+                        result['class'], result['scores'], class_names,scores_thresh=0.85)
             if cropped_file_folder:
                 ori_file_folder = cropped_file_folder
             result1 , result2, suggestions = predict_init(ori_file_folder,str_time)

main/views_utils.py
@@ -72,17 +72,8 @@ def image_save(im = None,str_time="default"):
     image_ori.save(file_folder)
 
     width_resize = 224
-
-    #save original size
-    #file_folder = os.path.join(MEDIA_DIR,str_time+'-ori.jpg')
-    #image = image.convert("RGB")
-    #stats = image.save(file_folder)
-
-    #save resized size for caffe
-    wpercent = (width_resize/float(image.size[0]))
-    height_resize  = int(float(image.size[1])*float(wpercent))
     file_folder = os.path.join(MEDIA_DIR,str_time+'.jpg')
-    image = image.resize((width_resize,height_resize),Image.ANTIALIAS)
+    image = image.resize((width_resize,width_resize),Image.ANTIALIAS)
     image = image.convert("RGB")
     image.save(file_folder)
 
@@ -179,10 +170,10 @@ def process_bounding_mask(bounding_box,mask):
     old_y1 = bounding_box[1]
     old_x2 = bounding_box[2]
     old_y2 = bounding_box[3]
-    new_x1 = bounding_box[0]  = max(old_x1-50,0)#x1
-    new_y1 = bounding_box[1]  = max(old_y1-50,0)#y1
-    new_x2 = bounding_box[2]  = min(old_x2+50,224)#x2
-    new_y2 = bounding_box[3]  = min(old_y2+50,224)#y2
+    new_x1 = bounding_box[0]  = max(old_x1-35,0)#x1
+    new_y1 = bounding_box[1]  = max(old_y1-35,0)#y1
+    new_x2 = bounding_box[2]  = min(old_x2+45,224)#x2
+    new_y2 = bounding_box[3]  = min(old_y2+45,224)#y2
 
     new_length = new_y2-new_y1
     new_height = new_x2-new_x1
@@ -191,19 +182,17 @@ def process_bounding_mask(bounding_box,mask):
     old_length = old_y2-old_y1
     old_height = old_x2-old_x1
     old_area = old_length*old_height
-    scale = new_area/old_area
+    scale = min(new_area/old_area,2)
 
-
-    mask = 255.0*mask[...,0]
-    mask = np.float32(mask)
-    ret, thresh = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
     thresh = np.uint8(thresh)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnt_scaled = scale_contour(contours[0], scale)
 
-    cv2.drawContours(mask, pts =[cnt_scaled], color=(255,255,255))
+    im_copy = mask.copy()
+    cv2.drawContours(im_copy, [cnt_scaled], 0, (255, 0, 0), 2)
+    cv2.fillPoly(im_copy, pts =[cnt_scaled], color=(255,255,255))
+    im_copy = im_copy[:,:,newaxis]
+    im_copy[im_copy >0]=True
 
-    mask = mask[:,:,newaxis]
-    mask[mask >0]=True
-
-    return bounding_box,mask
+    return bounding_box,im_copy